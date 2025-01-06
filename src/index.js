b2bae6302b934c088a828b153afc2343e772e407
Mvp1931 <mihirphadnis02@gmail.com>
2025-01-06 17:22:14 +0530

    completed Put and Patch requests

--- src/index.js
+++ src/index.js
@@ -46,7 +46,7 @@ const mockData = [
         name: "Anson Carter",
         email: "ansoncarter@example.com",
         displayName: "Anson",
-    }
+    },
 ];
 
 const mockItems = [
@@ -54,33 +54,42 @@ const mockItems = [
         id: 1,
         name: "Item 1",
         item: "chicken breast",
+        price: 12.99,
     },
     {
         id: 2,
         name: "Item 2",
         item: "apples",
+        price: 2.99,
     },
     {
         id: 3,
         name: "Item 3",
         item: "bananas",
+        price: 1.99,
     },
     {
         id: 4,
         name: "Item 4",
         item: "oranges",
+        price: 3.99,
     },
     {
         id: 5,
         name: "Item 5",
         item: "grapes",
+        price: 7.99,
     },
     {
         id: 6,
         name: "Item 6",
         item: "strawberries",
+        price: 8.99,
     },
 ];
+
+// set up middleware
+app.use(express.json());
 
 // set up routes
 // main route
@@ -96,18 +105,18 @@ app.get("/", (request, response) => {
 app.get("/api/users", (request, response) => {
     console.log(request.query);
     const {
-        query: { filter, value } 
-    }  = request;
+        query: { filter, value },
+    } = request;
     if (filter && value) {
-        return response.send( mockData.filter((user) => user[filter].includes(value)))
-    }  
-    response.send(mockData)
+        return response.send(mockData.filter((user) => user[filter].includes(value)));
+    }
+    response.send(mockData);
 });
 
 // to get user by id (using request params)
 app.get("/api/users/:id", (request, response) => {
-    // console.log(request.params);
-    
+    //console.log(request.params);
+
     const userID = parseInt(request.params.id);
     console.log(userID);
     if (isNaN(userID)) {
@@ -128,9 +137,65 @@ app.get("/api/items", (request, response) => {
 
 // Post Requests
 
-app.post("", (request, response) => {
-    
-})
+app.post("/api/users", (request, response) => {
+    console.log(request.body);
+    const { body } = request;
+
+    const newUser = {
+        id: mockData[mockData.length - 1].id + 1,
+        ...body,
+    };
+    mockData.push(newUser);
+    return response.status(201).send(newUser);
+});
+
+// Put Requests
+
+app.put("/api/users/:id", (request, response) => {
+    //console.log(request.body);
+    const {
+        body,
+        params: { id },
+    } = request;
+    const parsedID = parseInt(id);
+
+    if (isNaN(parsedID)) {
+        return response.status(400).send({
+            message: "Bad Request, Invalid user ID",
+        });
+    }
+    const findUserIndex = mockData.findIndex((user) => user.id === parsedID);
+
+    if (findUserIndex === -1) {
+        return response.status(404);
+    }
+
+    mockData[findUserIndex] = { id: parsedID, ...body };
+    return response.sendStatus(200);
+});
+
+// Patch Requests
+
+app.patch("/api/users/:id", (request, response) => {
+    const { body, params } = request;
+    const parsedID = parseInt(params.id);
+
+    if (isNaN(parsedID)) {
+        return response.status(400).send({
+            message: "Bad Request, Invalid user ID",
+        });
+    }
+    const findUserIndex = mockData.findIndex((user) => user.id === parsedID);
+    console.log(findUserIndex);
+
+    if (findUserIndex === -1) {
+        return response.sendStatus(404);
+    }
+
+    mockData[findUserIndex] = { ...mockData[findUserIndex], ...body }; // overwrite the user with the new updated data
+    console.log(mockData[findUserIndex]);
+    return response.sendStatus(200);
+});
 
 // server listening on port 3000
 app.listen(PORT, () => {
